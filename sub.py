2f4a29fb3f5053e113a6beaf9d36cc449fd1063b
maks <pierchala.maks@gmail.com>
2023-09-29 02:47:14 +0200

    Modify to work on multiple topics

--- sub.py
+++ sub.py
@@ -1,48 +1,59 @@
 import json
 import paho.mqtt.client as mqtt
-# Topic na ktory bedziemy wysylac nasze wiadomosci. Musza byc spojne na backendzie i tutaj.
-MQTT_TOPIC = "udalo/sie"
-MQTT_HOST = "example.com"
-MQTT_PORT = "Use 1883 for MQTT TCP/IP"
+MQTT_TOPIC_ANOTHER = "udalo/sie/request"
+MQTT_HOST = "165.232.98.202"
+MQTT_PORT = 1883
+MQTT_TOPIC_BOX = "uuid/7a2d934f-a373-4b2f-83ab-6593ce5a4f6b/request"
 
-# Ta funkcja wywolac sie powinna, gdy on_message sie wykona prawidlowo
-def on_publish(client, userdata, mid):
-    print("Przetworzone, wyslane response")
+response_functions = {
+    MQTT_TOPIC_BOX: lambda json_payload, topic: handle_topic_box(json_payload, topic),
+    MQTT_TOPIC_ANOTHER: lambda json_payload, topic: handle_another_topic_request(json_payload, topic)
+}
 
-# on_message ma za zadanie robic to, 
-# co powinna funkcja rpc.register (w ogromnym uproszczeniu)
-# z libki mqtt-json-rpc
-def on_message(client, userdata, message):
-
-    # Konwertujemy JSON na obiekt
-    json_payload = json.loads(message.payload)
-    method = json_payload['method']
-
-    # Wyciagamy potrzebne informacje, zeby wyslac response na poprawny topic
+def handle_create_topic(json_payload, topic: str):
     rpc_id: str = json_payload['id']
-    rid_cid = rpc_id.split(":")
-    cid = rid_cid[0]
+    cid_rid = rpc_id.split(":")
+    cid = cid_rid[0]
+    replaced_topic = topic.replace('/request', '/response')
+    response_topic = f"{replaced_topic}/{cid}"
+    return [response_topic, rpc_id]
 
-    # Tworzymy response topic 
-    response_topic = f"{method}/response/{cid}"
-
-    # Tak wyglada przyjmowany JSON przez nasze mqtt-json-rpc
+def handle_topic_box(json_payload, topic):
+    print("Dzialam")
+    [response_topic, rpc_id] = handle_create_topic(json_payload=json_payload, topic=topic)
     response = {
         "jsonrpc": "2.0",
         "id": rpc_id,
-        # Nasz result to response, który zostaje zwrócony przez rpc.call
+        "result": 6
+    }
+    return [response, response_topic]
+
+def handle_another_topic_request(json_payload, topic):
+    [response_topic, rpc_id] = handle_create_topic(json_payload=json_payload, topic=topic)
+    response = {
+        "jsonrpc": "2.0",
+        "id": rpc_id,
         "result": 6
     }
 
-    # Wysylamy wiadomosc na odpowiedni topic, z obiektem na JSON.
-    client_mqtt.publish(response_topic, json.dumps(response), qos=0)
+    return [response, response_topic]
+
+def on_publish(client, userdata, mid):
+    print("Przetworzone, wyslane response")
+
+def on_message(client, userdata, message):
+    json_payload = json.loads(message.payload)
+    topic = json_payload['method'] + "/request"
+    print(topic)
+    if topic in response_functions:
+        print()
+        [response, response_topic] = response_functions[topic](json_payload, topic)
+        client_mqtt.publish(response_topic, json.dumps(response), qos=0)
     
-# Jakies ustawienia, zeby sie polaczyc z serwerem
 client_mqtt = mqtt.Client()
 client_mqtt.on_message = on_message
 client_mqtt.on_publish = on_publish
 client_mqtt.connect(MQTT_HOST, MQTT_PORT, 60)
 
-# Subscribe, czyli oczekiwanie na topic o takiej postaci. Ta dwojka to qos=2
-client_mqtt.subscribe(f"{MQTT_TOPIC}/request", 2)
+client_mqtt.subscribe([(MQTT_TOPIC_BOX,2), (MQTT_TOPIC_ANOTHER, 2)])
 client_mqtt.loop_forever()
